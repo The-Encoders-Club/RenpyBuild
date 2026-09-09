06f1d65ac7b81a784f09b1936da086eb592fe2ab
Slytharbez <aranmateo@proton.me>
2026-09-08 22:38:09 -0500

    module: fix inverted arguments in IMG_SavePNG_RW call
    - Correct save_png_core call signature to IMG_SavePNG_RW(surf, rw, 0).
    - Matches official SDL2_image signature: IMG_SavePNG_RW(SDL_Surface*, SDL_RWops*, int).
    - Fixes SIGSEGV (SEGV_MAPERR) crash on ARM64 during first-frame screenshot/render.

--- module/core.c
+++ module/core.c
@@ -31,7 +31,7 @@ void save_png_core(PyObject *pysurf, SDL_RWops *rw, int compress) {
     surf = PySurface_AsSurface(pysurf);
 
     /* Can't release GIL, since we're not using threaded RWops. */
-    IMG_SavePNG_RW(rw, surf, compress);
+    IMG_SavePNG_RW(surf, rw, 0);
 }
 
 /* This pixellates a 32-bit RGBA pygame surface to a destination
