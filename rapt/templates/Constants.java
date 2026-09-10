8889484b6e202aa97cab81a2f3d267461f8e54d1
Slytharbez <aranmateo@proton.me>
2026-09-10 16:30:09 -0500

    feat(rapt): migrate build pipeline from legacy Ant to modern Gradle

--- rapt/templates/Constants.java
+++ rapt/templates/Constants.java
@@ -1,14 +1,14 @@
 package org.renpy.android;
 
 public class Constants {
-	// Used by the google play store.
-    public static String PLAY_BASE64_PUBLIC_KEY = "{{ config.google_play_key }}";
-    public static byte[] PLAY_SALT = new byte[] { {{ config.google_play_salt }} };
-
-    // Used by the expansion downloader.
-    public static int fileVersion = {{ config.numeric_version }};
-    public static int fileSize = {{ file_size }};
-
     // Used by the in-app purchasing code.
     public static String store = "{{ config.store }}";
+
+    // The names of the asset packs to check and download.
+{% if big_bundle %}
+    public static String assetPacks[ ] = { "ff1", "ff2", "ff3", "ff4" };
+{% else %}
+    public static String assetPacks[ ] = { };
+{% endif %}
+
 }
